97eac03e3517b307ddb445027526a2866462bad6
Get Regged <148784927+GetRegged@users.noreply.github.com>
2024-10-07 15:25:56 +0200

    Delete Bitsum-Highest-Performance.bat

--- Bitsum-Highest-Performance.bat
+++
@@ -1,3 +0,0 @@
-curl -g -k -L -# -o "%temp%\Bitsum-Highest-Performance.pow" "https://github.com/GetRegged/GetRegged-Windows-10-22H2-Ultimate-Gaming-Registry/raw/main/bin/Bitsum-Highest-Performance.pow"
-powercfg -import "%temp%\Bitsum-Highest-Performance.pow" 11111111-1111-1111-1111-111111111111
-powercfg -setactive 11111111-1111-1111-1111-111111111111
