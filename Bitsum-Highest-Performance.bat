6624a4ac0ce139caad7a880d2f6bb077e8713ff8
Get Regged <148784927+GetRegged@users.noreply.github.com>
2024-10-07 15:15:52 +0200

    Add files via upload

---
+++ Bitsum-Highest-Performance.bat
@@ -0,0 +1,3 @@
+curl -g -k -L -# -o "%temp%\Bitsum-Highest-Performance.pow" "https://github.com/GetRegged/GetRegged-Windows-10-22H2-Ultimate-Gaming-Registry/raw/main/Bitsum-Highest-Performance.pow"
+powercfg -import "%temp%\Bitsum-Highest-Performance.pow" 11111111-1111-1111-1111-111111111111
+powercfg -setactive 11111111-1111-1111-1111-111111111111
